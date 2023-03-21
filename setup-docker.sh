51d1443a904f691f0a1720fcc25cadef9d801aaf
Eduardo Dluhosch <40745408+dmaax@users.noreply.github.com>
2023-03-21 18:45:57 -0300

    Create setup-docker.sh

---
+++ setup-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Docker and docker-compose (3.8) install script that works on Ubuntu Server 20.04
+
+user="userhere"
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce -y
+sudo systemctl status docker
+
+curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
+sudo usermod -aG docker $user
