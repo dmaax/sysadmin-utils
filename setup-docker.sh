102ca974f07a9930a5fa6535da81bc1794bdbd4a
Eduardo Dluhosch <40745408+dmaax@users.noreply.github.com>
2023-03-21 18:50:12 -0300

    Make docker-compose binary executable

--- setup-docker.sh
+++ setup-docker.sh
@@ -12,5 +12,6 @@ sudo apt install docker-ce -y
 sudo systemctl status docker
 
 curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo usermod -aG docker $user
